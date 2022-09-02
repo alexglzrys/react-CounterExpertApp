e247828bff7de8fdf8571ae644a2a0f518ad45cc
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-02 18:58:07 -0500

    primer componente de React

---
+++ README.md
@@ -0,0 +1,15 @@
+## Vite + React
+
+A diferencia de Create React App que usa Webpack, Vite es más rápido en cuanto a Hot Module Reload y configuración.
+
+Crear un proyecto de React con Vite
+```
+npm create vite@latest
+```
+
+El asistente preguntará por el nombre de la aplicación y si usará JS Vanilla o alguna librería o framework de JS para trabajar, del mismo modo, preguntará si se desea usar JS o TS.
+
+Para ejecutar el proyecto basta con el comando
+```
+npm run dev
+```
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// Importación de librería React y ReactDOM para trabajar en el DOM del navegador
+import React from 'react';
+import ReactDOM from 'react-dom'
 
+// Declaración de componente funcional
+function App() {
+    // Retornar código JSX - Vista del componente
+    return (<h1>Hola Mundo desde Rect con Vite.</h1>)
+}
+
+// Renderizar el componente en el DOM del navegador
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    // Se recomienda encarecidamente trabajar siempre en modo estricto
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
