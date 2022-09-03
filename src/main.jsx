b4b5d43cbefa877a66370a25b25e788f0eb224d5
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-02 20:08:19 -0500

    trabajar con CSS en React

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import ReactDOM from 'react-dom/client'
 import App from './HelloWorldApp';
 import FirstApp from './FirstApp';
 
+// Para estilos CSS Globales, se importan directamente en el archivo principal de la aplicación
+// Se puede usar CSS, SASS, LESS, STYLUS, Styles Components, etc. en React - Pero es importante instalar el paquete previamente si se esta trabajando con un preprocesador de CSS, así como configurarlo en el constructor de paquetes (Create-React-App ya viene configruados por defecto), en Vite se encarga tras bambalinas de ello, sin embargo, si trabajamos directamente con Webpack, habra que buscar el loader y configurarlo a mano.
+import './App.css';
+
 // Toda aplicación de React tiene un punto de entrada. Por lo general es el archivo main
 
 
