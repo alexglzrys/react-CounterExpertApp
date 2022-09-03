0fe325638adf9c09151eb93b88dd85caf8ec7a88
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-03 09:50:59 -0500

    useState - hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,17 +1,38 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 // Si la función manejadora de evento, no usa los props, se recomienda colocarla fuera del cuerpo de la función del componente 
-const handleAdd = (event) => {
+/*const handleAdd = (event) => {
   console.log(event)
-}
+}*/
 
 function CounterApp({ value }) {  
+  // Declarar e inicializar el estado de este componente
+  // counter = "el valor pasado como prop"
+  const [counter, setCounter] = useState(value);
+  
+  const handleAdd = (event) => {
+    // Modificar el estado del componente
+    setCounter(counter + 1);
+    // setCounter((counter) => counter + 1)
+  }
+
+  const handleSubtract = (event) => {
+    setCounter(counter - 1)
+  }
+
+  const handleReset = (event) => {
+    setCounter(value);
+  }
+  
   return (
     <>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
       {/* Reaccionar a eventos sintéticos de React, si la función no recibe parámetros adicionales, solo le menciona, pero el evento disparado se pasa implicitamente. Caso contrario, el evento pasa como el primer parámetro */}
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubtract}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 }
